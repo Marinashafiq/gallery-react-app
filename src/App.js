6257afd7972f3ce21fdfa74b3a3f64b54e4fb42d
Marina Magdy <Marina.Magdy@ibtikar.com>
2019-10-30 12:44:55 +0200

    react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button , Dropdown } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="primary">Primary</Button>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
