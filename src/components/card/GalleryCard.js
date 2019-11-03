9ec30225876855e6a26ac6c0d73afc1027048763
Marina Magdy <Marina.Magdy@ibtikar.com>
2019-11-03 11:39:04 +0200

    gallery grid added

--- src/components/card/GalleryCard.js
+++ src/components/card/GalleryCard.js
@@ -4,17 +4,17 @@ import Card from 'react-bootstrap/Card';
 const GalleryCard = (props) =>{
     return(
        <div>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+            <Card className="border-0 shadow mb-3">
+                <Card.Img variant="top" src={props.url} />
                 <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>{props.name}</Card.Title>
                     <Card.Text>
                         This is a wider card with supporting text below as a natural lead-in to
                         additional content. This content is a little bit longer.
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <small className="text-muted">{props.created_at}</small>
                 </Card.Footer>
             </Card>
        </div>
--- src/containers/Photos/PhotosList.js
+++ src/containers/Photos/PhotosList.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import { requestPhotos } from '../../store/actions/index';
+import GalleryCard from '../../components/card/GalleryCard';
+import { Container, Row, Col } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card'
+import CardColumns from 'react-bootstrap/CardColumns'
 
 class PhotosList extends React.Component {
+    
 
     componentDidMount() {
         console.log(this.props);
@@ -12,16 +17,21 @@ class PhotosList extends React.Component {
 
     renderPhotosList() {
         return this.props.photos.map(photo => {
-            console.log(photo.id);
+            console.log(photo.created_at);
+            photo.created_at = this.convertTime(photo.created_at);
+            console.log(photo.created_at);
             return (
-                <div  key={photo.id}>
-                    <div>
-                        <img src={photo.urls.full} height='100' width='100' />
-                        {photo.id}
-                    </div>
-                </div>
+
+                    <GalleryCard  key={photo.id} url={photo.urls.full} name={photo.id} created_at={photo.created_at} />
+              
             )
         })
+    }
+
+    convertTime(createdAt){
+        let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        let created_at_date = new Date(createdAt).toLocaleDateString('en-US', options);  
+       return created_at_date;       
     }
 
 
@@ -30,8 +40,13 @@ class PhotosList extends React.Component {
         console.log(this.props.photos);
         return (
             <div>
-              <h1>Photos List</h1>
-              {this.renderPhotosList()}
+                <h1>Photos List</h1>
+                <Container>
+                    <CardColumns>
+                        {this.renderPhotosList()}
+                    </CardColumns>
+                </Container>
+
             </div>
         )
     }
