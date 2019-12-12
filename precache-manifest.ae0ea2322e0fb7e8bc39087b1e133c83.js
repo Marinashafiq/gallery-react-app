1f612b1ccfb6f6bb0656aa5290cb108e813bb609
Marina shafiq <marinashafiq89@gmail.com>
2019-12-13 01:00:22 +0200

    Updates

---
+++ precache-manifest.ae0ea2322e0fb7e8bc39087b1e133c83.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d253bd0c324ae1ab1fe133220e8aaaf6",
+    "url": "/gallery-react-app/index.html"
+  },
+  {
+    "revision": "861b4b4ef5d16f866873",
+    "url": "/gallery-react-app/static/css/3.98911b94.chunk.css"
+  },
+  {
+    "revision": "1f74e27a43b461f14b78",
+    "url": "/gallery-react-app/static/css/5.a15bd2a3.chunk.css"
+  },
+  {
+    "revision": "9ab5b7cdc5da5105b40e",
+    "url": "/gallery-react-app/static/css/6.493877db.chunk.css"
+  },
+  {
+    "revision": "dc4c780f2a148719d622",
+    "url": "/gallery-react-app/static/css/7.ca3846b7.chunk.css"
+  },
+  {
+    "revision": "5e11fb54e327b3d40a0e",
+    "url": "/gallery-react-app/static/css/8.7b0eec42.chunk.css"
+  },
+  {
+    "revision": "cc2b5e87865a5522a1fa",
+    "url": "/gallery-react-app/static/css/main.1257b9da.chunk.css"
+  },
+  {
+    "revision": "284c96dd3bb41f03289f",
+    "url": "/gallery-react-app/static/js/0.66394dee.chunk.js"
+  },
+  {
+    "revision": "861b4b4ef5d16f866873",
+    "url": "/gallery-react-app/static/js/3.a61df0cb.chunk.js"
+  },
+  {
+    "revision": "83af5bc20be3c53ff7a1",
+    "url": "/gallery-react-app/static/js/4.bfb0a3b2.chunk.js"
+  },
+  {
+    "revision": "1f74e27a43b461f14b78",
+    "url": "/gallery-react-app/static/js/5.4342a250.chunk.js"
+  },
+  {
+    "revision": "9ab5b7cdc5da5105b40e",
+    "url": "/gallery-react-app/static/js/6.d28de3f1.chunk.js"
+  },
+  {
+    "revision": "dc4c780f2a148719d622",
+    "url": "/gallery-react-app/static/js/7.f2a66669.chunk.js"
+  },
+  {
+    "revision": "5e11fb54e327b3d40a0e",
+    "url": "/gallery-react-app/static/js/8.4875a71b.chunk.js"
+  },
+  {
+    "revision": "cc2b5e87865a5522a1fa",
+    "url": "/gallery-react-app/static/js/main.47fd505a.chunk.js"
+  },
+  {
+    "revision": "a5eae365781cf7cf7b15",
+    "url": "/gallery-react-app/static/js/runtime-main.554bf0e9.js"
+  },
+  {
+    "revision": "e05090b757da107c30910405cdf6154a",
+    "url": "/gallery-react-app/static/media/bodyBg.e05090b7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gallery-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gallery-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gallery-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gallery-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gallery-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
